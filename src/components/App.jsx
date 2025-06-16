6a197779b82f5f346339b57f088d20f958fa0735
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-16 11:10:12 +0200

    Varcel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,6 @@ import { PhonebookInput } from './PhonebookInput';
 import { ContactList } from './ContactList';
 
 export const App = () => {
-  const handleDelete = () => dispatch(deleteContact(contact.id));
-
-  const handleFilterChange = evt => {};
-
-  // const visibleContacts = state.contacts.filter(contact =>
-  //   contact.name.toLowerCase().includes((state.filter || '').toLowerCase())
-  // );
   return (
     <div>
       <Section title="Phonebook">
